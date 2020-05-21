552c6fcdc953ad42e7314bb6c4e6ba5555f7f4c4
Brett Saxon <brettcsaxon@gmail.com>
2020-05-21 15:35:58 -0400

    App update

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './routes/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import Favorites from './screens/Favorites';
-import Cart from './screens/Cart';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Navigator />
-      <Tab.Navigator>
-        {/* <Tab.Screen name='Home' component={Home} /> */}
-        <Tab.Screen name='Favs' component={Favorites} />
-        <Tab.Screen name='Cart' component={Cart} />
-      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/home.js
+++ screens/home.js
@@ -1,22 +1,50 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from 'react-native';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import { MyCarousel } from '../components/carousel';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Card from '../shared/card';
 
+import Favorites from '../screens/favorites';
+import Cart from '../screens/cart';
+
 export default function Home({ navigation }) {
+  const Tab = createBottomTabNavigator();
   const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { beerName: '', abv: 5, body: 'lorem ipsum', key: '1' },
-    { beerName: 'Gotta Catch Them All (again)', abv: 6, body: 'lorem ipsum', key: '2' },
-    { beerName: 'Not So "Final" Fantasy', abv: 9, body: 'lorem ipsum', key: '3' },
+    {
+      beerName: 'Gotta Catch Them All (again)',
+      abv: 6,
+      body: 'lorem ipsum',
+      key: '2',
+    },
+    {
+      beerName: 'Not So "Final" Fantasy',
+      abv: 9,
+      body: 'lorem ipsum',
+      key: '3',
+    },
   ]);
 
   return (
     <View style={globalStyles.container}>
       <MyCarousel></MyCarousel>
+
+      <Tab.Navigator>
+        {/* <Tab.Screen name='Home' component={Home} /> */}
+        <Tab.Screen name='Favs' component={Favorites} />
+        <Tab.Screen name='Cart' component={Cart} />
+      </Tab.Navigator>
     </View>
   );
 }
@@ -38,5 +66,5 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     flex: 1,
-  }
+  },
 });
